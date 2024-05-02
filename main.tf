9c7848183ca58e49f62c34c92dc12ff353d3dac1
sandeep <munagalasandeepreddy@gamail.com>
2024-05-02 10:50:08 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
  module "instance" {
    for_each = var.components
-   source = "git:: https://github.com/sandeepreddymunagala/tf-module-app.git"
+   source = "git::https://github.com/sandeepreddymunagala/tf-module-app.git"
    component = each.key
    env = var.env
  }
