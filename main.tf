360213b6e8cd01f05b96170211b9048e1a2ddc00
sandeep <munagalasandeepreddy@gamail.com>
2024-05-10 15:23:40 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -8,8 +8,11 @@
 
  module "vpc" {
     source = "git::https://github.com/sandeepreddymunagala/tf-module-vpc.git"
+
     for_each   = var.vpc
     cidr_block = each.value["cidr_block"]
+    subnets = each.value["subnets"]
+
     env = var.env
     tags = var.tags
     web_subnet_cider_block = each.value["web_subnet_cider_block"]
