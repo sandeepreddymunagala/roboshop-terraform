c4427222dbbf78d27ca8bfae225277b65ce34eff
sandeep <munagalasandeepreddy@gamail.com>
2024-05-11 09:42:36 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "vpc" {
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
   subnets = each.value["subnets"]
+  default_vpc_id = var.default_vpc_id
 
   env = var.env
   tags = var.tags
