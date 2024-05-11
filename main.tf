ec79f23a1f3bc7b3a5d993af1ce33ae47bdc3bbe
sandeep <munagalasandeepreddy@gamail.com>
2024-05-11 10:18:59 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -18,4 +18,14 @@ module "vpc" {
   tags = var.tags
 }
 
+ module "app_server" {
+   source = "git::https://github.com/sandeepreddymunagala/tf-module-app.git"
+
+   env = var.env
+   tags =var.tags
+   component = test
+   subnet_id = lookup (lookup (lookup(lookup (module.vpc, "main", null), "subnet ids", null), "app", null), "subnet ids",
+   null)[0]
+   vpc_id = lookup (lookup (module.vpc, "main", null), "vpc id", null)
+ }
 
