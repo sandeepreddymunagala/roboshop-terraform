306629e1d3f0c8ecc77344d0a6f8363424d3dcf9
sandeep <munagalasandeepreddy@gamail.com>
2024-05-14 10:12:14 +0200

    learn terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -57,3 +57,10 @@ vpc = {
 
 default_vpc_id        = "vpc-053c37932ccf923f5"
 default_vpc_rt        = "rtb-0a2b1e95368e669b9"
+
+rabbitmq = {
+  main = {
+    instance_type = "t2.micro"
+    component     = "rabbitmq"
+  }
+}
--- main.tf
+++ main.tf
@@ -31,3 +31,21 @@ module "app_server" {
   vpc_id = lookup (lookup (module.vpc, "main", null), "vpc_id", null)
 }
 
+
+module "rabbitmq" {
+  source = "git::https://github.com/sandeepreddymunagala/tf-module-rabbitmq.git"
+
+  for_each      = var.rabbitmq
+  component     = each.value["component"]
+  instance_type = each.value["instance_type"]
+
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  subnet_id      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)[0]
+
+  env            = var.env
+  tags           = var.tags
+
+}
+
+
