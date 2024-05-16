704d4841d57f90792b149c9284573a4f2abbb4e3
sandeep <munagalasandeepreddy@gamail.com>
2024-05-16 10:24:28 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -120,20 +120,9 @@ module "rabbitmq" {
    load_balancer_type = each.value[""]
    vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
    sg_subnet_cidr     = each.value["name"] == "public" ? ["0.0.0.0/0"] : local.app_web_subnet_cidr
-   subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], n
+   subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
 
 
    env  = var.env
    tags = var.tags
  }
- /  for_each           = var.alb
- //  name               = each.value["name"]
- //  internal           = each.value["internal"]
- //  load_balancer_type = each.value["load_balancer_type"]
- //  vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
- //  sg_subnet_cidr     = each.value["name"] == "public" ? ["0.0.0.0/0"] : local.app_web_subnet_cidr
- //  subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
- //
- //  env  = var.env
- //  tags = var.tags
- //}
