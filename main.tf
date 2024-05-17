e95c2bd5867e544a9044b8209ff15613e33b4c59
sandeep <munagalasandeepreddy@gamail.com>
2024-05-17 09:18:25 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -36,9 +36,9 @@ module "apps" {
   env                   = var.env
   tags                  = var.tags
   kms_key_id            = var.kms_key_arn
-  allow_ssh_cidr        = var.allow_ssh_cidr
-  kms_arn               = var.kms_key_arn
-  allow_prometheus_cidr = var.allow_prometheus_cidr
+  //  allow_ssh_cidr        = var.allow_ssh_cidr
+  //  kms_arn               = var.kms_key_arn
+  //  allow_prometheus_cidr = var.allow_prometheus_cidr
 
 }
 
