47a9228acdba81419f5a8e47f896352234f3d676
sandeep <munagalasandeepreddy@gamail.com>
2024-05-17 09:49:22 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -32,6 +32,8 @@ module "apps" {
   component          = each.value["component"]
   subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
   vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  lb_dns_name        = lookup(lookup(module.alb, each.value["lb_ref"], null), "dns_name", null)
+  listener_arn       = lookup(lookup(module.alb, each.value["lb_ref"], null), "listener_arn", null)
 
   env                   = var.env
   tags                  = var.tags
@@ -122,7 +124,7 @@ module "rabbitmq" {
    kms_key_arn = var.kms_key_arn
  }*/
 
- module "aln" {
+ module "alb" {
    source = "git::https://github.com/sandeepreddymunagala/tf-module-alb.git"
 
    for_each = var.alb
