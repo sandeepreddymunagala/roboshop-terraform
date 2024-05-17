ee95eec937ddb18b725655851274377e1390a9e8
sandeep <munagalasandeepreddy@gamail.com>
2024-05-17 23:31:04 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "vpc" {
   source = "git::https://github.com/sandeepreddymunagala/tf-module-vpc.git"
 
-  for_each = var.vpc
-  cidr_block =each.value["cidr_block"]
-  subnets = each.value["subnets"]
+  for_each   = var.vpc
+  cidr_block = each.value["cidr_block"]
+  subnets    = each.value["subnets"]
 
-  env =var.env
-  tags =var.tags
+  env            = var.env
+  tags           = var.tags
   default_vpc_id = var.default_vpc_id
   default_vpc_rt = var.default_vpc_rt
-}
 
+}
 module "app" {
   source = "git::https://github.com/sandeepreddymunagala/tf-module-app.git"
 
